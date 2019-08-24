5e2abd99eccbd19982daa2a524d0f606c4e545bb
MW <minnowware@hotmail.com>
2019-08-24 23:26:40 +0200

    remove EDK2 directory

--- edk2/clrCdePkgBuild.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo.
-echo %0: deleting CdePkg from Build and from FV
-for /F %%d in ('dir /s /b /AD build\cde*') do rd /s /q %%d 
-ping 127.0.0.0 -n 4 > NUL
--- edk2/dbg.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /F %%a in ('dir /s /b secmain.exe') do (
-    cd %%~pa
-    secmain.exe /debug
-)
